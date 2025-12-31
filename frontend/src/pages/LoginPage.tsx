af783f43304d195b32455115dc16fb89afaf1c67
Anh D Tran <tran3ah@mail.uc.edu>
2025-12-31 02:08:34 -0500

    Grid cursor effects for auth page

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,10 +1,19 @@
 import { LoginForm } from "@/components/login-form"
+import GridBackground from "@/components/GridBackground"
 
 const LoginPage = () => {
   return (
-    <div className="flex justify-center items-center pt-10 md:pt-25 mx-10 md:mx-0">
-      <LoginForm />
-    </div>
+    <>
+      <GridBackground
+        glowColor="#a855f7"
+        glowRadius={180}
+        glowIntensity={0.3}
+        gridSize={32}
+      />
+      <div className="relative z-10 flex justify-center items-center pt-10 md:pt-25 mx-10 md:mx-0">
+        <LoginForm />
+      </div>
+    </>
   )
 }
 
--- frontend/src/pages/SignupPage.tsx
+++ frontend/src/pages/SignupPage.tsx
@@ -1,10 +1,19 @@
 import { SignupForm } from "@/components/signup-form"
+import GridBackground from "@/components/GridBackground"
 
 const SignupPage = () => {
   return (
-    <div className="flex justify-center items-center pt-10 mx-10 md:mx-0">
-      <SignupForm />
-    </div>
+    <>
+      <GridBackground
+        glowColor="#a855f7"
+        glowRadius={180}
+        glowIntensity={0.3}
+        gridSize={32}
+      />
+      <div className="relative z-10 flex justify-center items-center pt-10 mx-10 md:mx-0">
+        <SignupForm />
+      </div>
+    </>
   )
 }
 
