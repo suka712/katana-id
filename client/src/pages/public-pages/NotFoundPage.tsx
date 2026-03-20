9f38ac3baea9269c8bd9568d56d261588783cc63
khiem <khiemnguyen.hye@gmail.com>
2026-03-20 08:21:30 +0700

    Add github link

--- client/src/pages/public-pages/NotFoundPage.tsx
+++ client/src/pages/public-pages/NotFoundPage.tsx
@@ -5,7 +5,7 @@ const NotFoundPage = () => {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center gap-4 text-center">
       <h1 className="text-8xl font-heading bg-linear-to-r from-violet-400 via-indigo-400 to-cyan-400 bg-clip-text text-transparent italic">404</h1>
-      <span className="text-lg">This is weird. <a className="text-muted-foreground text-lg hover:underline" href="">Report issue.</a></span>
+      <span className="text-lg">This page doesn't exist. <a className="text-muted-foreground text-lg hover:underline" href="https://github.com/suka712/katana-id">Report issue.</a></span>
       <Button asChild>
         <Link to="/">Go home</Link>
       </Button>
